0ee84a37f14df4e778ab159a720c993813b40c79
reidfluegel <reid.fluegel@gmail.com>
2023-07-10 11:33:19 -0500

    add CRUD for posts

--- src/routes/posts.ts
+++ src/routes/posts.ts
@@ -1,4 +1,3 @@
-/** source/routes/posts.ts */
 import express from 'express';
 import controller from '../controllers/posts';
 const router = express.Router();
--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,3 @@
-/** source/server.ts */
 import http from 'http';
 import express, { Express } from 'express';
 import morgan from 'morgan';
@@ -6,20 +5,13 @@ import routes from './routes/posts';
 
 const router: Express = express();
 
-/** Logging */
 router.use(morgan('dev'));
-/** Parse the request */
 router.use(express.urlencoded({ extended: false }));
-/** Takes care of JSON data */
 router.use(express.json());
 
-/** RULES OF OUR API */
 router.use((req, res, next) => {
-    // set the CORS policy
     res.header('Access-Control-Allow-Origin', '*');
-    // set the CORS headers
     res.header('Access-Control-Allow-Headers', 'origin, X-Requested-With,Content-Type,Accept, Authorization');
-    // set the CORS method headers
     if (req.method === 'OPTIONS') {
         res.header('Access-Control-Allow-Methods', 'GET PATCH DELETE POST');
         return res.status(200).json({});
@@ -27,10 +19,8 @@ router.use((req, res, next) => {
     next();
 });
 
-/** Routes */
 router.use('/', routes);
 
-/** Error handling */
 router.use((req, res) => {
     const error = new Error('not found');
     return res.status(404).json({
@@ -38,7 +28,6 @@ router.use((req, res) => {
     });
 });
 
-/** Server */
 const httpServer = http.createServer(router);
 const PORT: any = process.env.PORT ?? 8080;
 httpServer.listen(PORT, () => console.log(`The server is running on port ${PORT}`));
