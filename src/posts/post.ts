1ebe4364b48e173fd3037fcf3d2f380d69a47e6c
reidfluegel <reid.fluegel@gmail.com>
2023-07-18 16:53:42 -0500

    alphabetize post properties

--- src/posts/post.ts
+++ src/posts/post.ts
@@ -1,15 +1,15 @@
 import {v4 as uuidv4} from 'uuid'
 
 export default class Post {
-    userId
     id
-    title
     body
+    title
+    userId
 
     constructor(userId = '', title = '', body = '') {
-        this.userId = userId
         this.id = uuidv4()
-        this.title = title
         this.body = body
+        this.title = title
+        this.userId = userId
     }
 }
