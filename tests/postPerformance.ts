3231dc27a320fae8afebc1f04e1bfeffbfa3a271
reidfluegel <reid.fluegel@gmail.com>
2023-08-13 11:26:26 -0500

    change perforance test body to be body

--- tests/postPerformance.ts
+++ tests/postPerformance.ts
@@ -9,7 +9,7 @@ export const options = {
 
 export default function () {
     const postBody = {userId: 'the user', title: 'the title', body: 'the body'}
-    const putBody = {userId: 'the new user', title: 'the new title', body: 'the new message!'}
+    const putBody = {userId: 'the new user', title: 'the new title', body: 'the new body'}
     const postResponse = http.post(`http://127.0.0.1:8080/posts/`, postBody)
     const id = JSON.parse(postResponse.body).message.id
     http.get(`http://127.0.0.1:8080/posts/${id}`)
