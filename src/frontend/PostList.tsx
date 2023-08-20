4d80a9df062ef82507524d1e17a0d5142eafb53e
reidfluegel <reid.fluegel@gmail.com>
2023-08-20 16:47:04 -0400

    add table to PostList page and error handling

--- src/frontend/PostList.tsx
+++ src/frontend/PostList.tsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import axios from 'axios'
 
 const PostList = () => {
+    const [posts, setPosts] = useState([])
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        // Fetch all posts from the backend
+        axios.get('http://127.0.0.1:8080/posts')
+            .then((response) => {
+                setPosts(response.data)
+                setLoading(false)
+            })
+            .catch((error) => {
+                console.error('An error occurred while fetching the posts:', error)
+                setLoading(false)
+            })
+    }, [])
+
 
     return (
-        <div>Loading...</div>
+        <div>
+            {loading
+                ? <div>Loading...</div>
+                : <table>
+                    <tbody>
+                    {posts.map((post: any) => (
+                        <tr key={post.id}>
+                            <td>{post.id}</td>
+                            <td>{post.userId}</td>
+                            <td>{post.title}</td>
+                            <td>{post.body}</td>
+                        </tr>
+                    ))}
+                    </tbody>
+                </table>}
+        </div>
     )
 }
 export default PostList
--- tests/frontend/PostList.spec.tsx
+++ tests/frontend/PostList.spec.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {act, render, screen} from '@testing-library/react'
+import {render, screen, waitFor} from '@testing-library/react'
 import PostList from '../../src/frontend/PostList'
 import axios from 'axios'
 
@@ -7,19 +7,52 @@ jest.mock('axios', () => ({
     get: jest.fn()
 }))
 
+beforeAll(() => {
+    const posts = [
+        {id: 1, userId: 'User 1', title: 'Post 1', body: 'Body 1'},
+        {id: 2, userId: 'User 2', title: 'Post 2', body: 'Body 2'}
+    ];
+    (axios.get as jest.Mock).mockResolvedValue({data: posts})
+
+})
 
 describe('PostsList component', () => {
-    test('renders loading message initially', () => {
-        //given
-        (axios.get as jest.Mock).mockResolvedValueOnce({data: {}})
-
+    test('renders loading message initially', async () => {
         // when
-        act(() => {
-            render(<PostList/>)
-        })
+        render(<PostList/>)
 
         // then
-        expect(screen.getByText('Loading...')).toBeInTheDocument()
+        await waitFor(() => {
+            expect(screen.getByText('Loading...')).toBeInTheDocument()
+        })
 
+    })
+
+    test('fetches and displays posts', async () => {
+        // when
+        render(<PostList/>)
+
+        // then
+        await waitFor(() => {
+            expect(screen.getByText('Post 1')).toBeInTheDocument()
+            expect(screen.getByText('User 1')).toBeInTheDocument()
+            expect(screen.getByText('Body 1')).toBeInTheDocument()
+            expect(screen.getByText('Post 2')).toBeInTheDocument()
+            expect(screen.getByText('User 2')).toBeInTheDocument()
+            expect(screen.getByText('Body 2')).toBeInTheDocument()
+        })
+    })
+
+    test('displays an error if fetching posts fails', async () => {
+        let error: Error = new Error('An error occurred');
+        (axios.get as jest.Mock).mockRejectedValueOnce(error)
+        console.error = jest.fn()
+
+        render(<PostList/>)
+
+        await waitFor(() => {
+            expect(screen.queryByText('Loading...')).not.toBeInTheDocument()
+            expect(console.error).toHaveBeenCalledWith('An error occurred while fetching the posts:', error)
+        })
     })
 })
