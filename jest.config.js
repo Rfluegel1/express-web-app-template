402271e0e596fe5bd1ccb90ddae51a1798a27db5
reidfluegel <reid.fluegel@gmail.com>
2023-08-21 10:51:49 -0400

    revert jest config to one project

--- jest.config.js
+++ jest.config.js
@@ -1,25 +1,11 @@
 module.exports = {
-    projects: [
-        {
-            displayName: 'backend',
-            testEnvironment: 'node',
-            testMatch: ['<rootDir>/tests/backend/**/*.spec.ts'],
-            moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-            globalSetup: '<rootDir>/tests/backend/globalSetup.ts',
-            globalTeardown: '<rootDir>/tests/backend/globalTeardown.ts',
-            transform: {
-                '^.+\\.jsx?$': 'babel-jest', '^.+\\.tsx?$': 'ts-jest'
-            },
-        },
-        {
-            displayName: 'frontend',
-            testEnvironment: 'jsdom',
-            testMatch: ['<rootDir>/tests/frontend/**/*.spec.tsx'],
-            moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-            transform: {
-                '^.+\\.jsx?$': 'babel-jest', '^.+\\.tsx?$': 'ts-jest'
-            },
-            setupFilesAfterEnv: ['<rootDir>/tests/frontend/jest.setup.ts'],
-        },
-    ],
+    displayName: 'backend',
+    testEnvironment: 'node',
+    testMatch: ['<rootDir>/tests/backend/**/*.spec.ts'],
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+    globalSetup: '<rootDir>/tests/backend/globalSetup.ts',
+    globalTeardown: '<rootDir>/tests/backend/globalTeardown.ts',
+    transform: {
+        '^.+\\.jsx?$': 'babel-jest', '^.+\\.tsx?$': 'ts-jest'
+    }
 };
