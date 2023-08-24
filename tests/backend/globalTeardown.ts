1595317d54d95fa0b3715b8a2780c64236639827
reidfluegel <reid.fluegel@gmail.com>
2023-08-24 10:06:26 -0400

    more strict kill in teardown

--- tests/backend/globalTeardown.ts
+++ tests/backend/globalTeardown.ts
@@ -2,14 +2,19 @@ import fs from 'fs'
 import path from 'path'
 import {spawn} from 'child_process'
 
-export default async () => new Promise((resolve) => {
+export default async () => new Promise((resolve, reject) => {
     let filepath = path.join(__dirname, 'server.pid')
     const pid = fs.readFileSync(filepath).toString()
     console.log('killing pid: ', pid)
-    const server = spawn('kill', ['--KILL', pid])
-    server.stdout.on('close', () => {
-        console.log('killed')
-        fs.unlinkSync(filepath)
-        resolve(server)
+    const server = spawn('kill', ['-9', pid])
+
+    server.on('exit', (code) => {
+        if (code === 0) {
+            console.log('killed')
+            fs.unlinkSync(filepath)
+            resolve(server)
+        } else {
+            reject(new Error('Failed to kill process'))
+        }
     })
 });
