984298f9fedf687414c90ab5470068905e127644
reidfluegel <reid.fluegel@gmail.com>
2023-08-28 19:23:22 -0400

    change NODE_ENV to development when it is otherwise test when using jest

--- tests/frontend/jest.setup.ts
+++ tests/frontend/jest.setup.ts
@@ -1,4 +1,7 @@
 import '@testing-library/jest-dom'
 
+if (process.env.NODE_ENV === 'test') {
+    process.env.NODE_ENV = 'development'
+}
 const env = process.env.NODE_ENV || 'development'
 require('dotenv').config({path: `.env.${env}`})
