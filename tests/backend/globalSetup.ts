3511235df92f6f097ad73f7b3156e72f6e846c26
reidfluegel <reid.fluegel@gmail.com>
2023-09-01 10:15:10 -0500

    make tests able to run individually though webstorm

--- tests/backend/globalSetup.ts
+++ tests/backend/globalSetup.ts
@@ -2,9 +2,15 @@ import {spawn} from 'child_process'
 import * as path from 'path'
 import * as fs from 'fs'
 
+if (process.env.NODE_ENV === 'test') {
+    process.env.NODE_ENV = 'development'
+}
+const env = process.env.NODE_ENV || 'development'
+require('dotenv').config({path: `.env.${env}`})
+
 export default async () => {
     const startBackend = () => new Promise((resolve) => {
-        const server = spawn('ts-node', ['src/backend/server.ts'])
+        const server = spawn('./node_modules/.bin/ts-node', ['src/backend/server.ts'])
         let pid = server.pid?.toString() ? server.pid?.toString() : 'pid undefined'
         console.log('created pid : ', pid)
         fs.writeFileSync(path.join(__dirname, 'server.pid'), pid)
--- tests/backend/jest.setup.ts
+++
@@ -1,5 +0,0 @@
-if (process.env.NODE_ENV === 'test') {
-    process.env.NODE_ENV = 'development'
-}
-const env = process.env.NODE_ENV || 'development'
-require('dotenv').config({path: `.env.${env}`})
