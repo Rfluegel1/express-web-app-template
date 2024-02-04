137dacd06c021ce30fa56b8e60df77a2560b399f
Rfluegel1 <reid.fluegel@gmail.com>
2024-02-03 19:20:27 -0600

    Delete backend/src/exceptions/badRequestException.ts

--- backend/src/exceptions/badRequestException.ts
+++
@@ -1,6 +0,0 @@
-export class BadRequestException extends Error {
-    constructor(id: string) {
-        super(`Parameter id not of type UUID for id=${id}`)
-        this.name = 'BadRequestException'
-    }
-}
