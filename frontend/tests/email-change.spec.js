b3b37bdfd18a8cd6ec16b4a7933d0d4f032f1ad9
reidfluegel <reid.fluegel@gmail.com>
2024-02-08 20:23:54 -0600

    add tests for when smtp enabled

--- frontend/tests/email-change.spec.js
+++ frontend/tests/email-change.spec.js
@@ -8,4 +8,59 @@ test.describe('Email change page', () => {
 		// expect
 		await expect(page.locator('h1')).toHaveText('Login');
 	});
+
+	test.skip('should call to email change for logged in user', async ({ page, context }) => {
+		// given
+		if (process.env.IS_GITHUB) {
+			await context.route('**/request-email-change', (route) => {
+				route.fulfill({
+					status: 204
+				});
+			});
+		}
+		const email = 'new.email@web-app-template.dev';
+		const requestPromise = page.waitForRequest('**/request-email-change');
+		await loginTestUser(page);
+
+		// expect
+		await expect(page.locator('h1')).toHaveText('Todo List');
+
+		// when
+		await page.goto('/email-change');
+		await page.fill('input[type="email"]', email);
+		await page.click('button[type="submit"]');
+
+		// then
+		const request = await requestPromise;
+		await page.waitForSelector(`text="Request sent to ${email} with further instructions"`);
+		await expect(request.url()).toMatch(/\/request-email-change$/);
+		await expect(page.locator('input[type=email]')).toHaveValue('');
+
+		// when
+		await page.click('a[href="/"]');
+
+		// then
+		await expect(page.locator('h1')).toHaveText('Todo List');
+	});
+
+	test.skip('should display error message when request fails', async ({ page, context }) => {
+		// given
+		await context.route('**/request-email-change', (route) => {
+			route.fulfill({
+				status: 500
+			});
+		});
+		await loginTestUser(page);
+
+		// expect
+		await expect(page.locator('h1')).toHaveText('Todo List');
+
+		// when
+		await page.goto('/email-change');
+		await page.fill('input[type="email"]', 'test.user@temporary.com');
+		await page.click('button[type="submit"]');
+
+		// then
+		await page.waitForSelector(`text="Something went wrong. Please try again."`);
+	});
 });
