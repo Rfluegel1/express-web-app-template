59a23844a3782bf99ddb6dcc4a5142788f94ac18
reidfluegel <reid.fluegel@gmail.com>
2024-02-20 11:16:19 -0600

    log request ip

--- backend/src/todos/todoController.ts
+++ backend/src/todos/todoController.ts
@@ -29,8 +29,8 @@ export default class TodoController {
 
     async createTodo(request: Request, response: Response, next: NextFunction) {
         getLogger().info('Received create todos request', {requestBody: request.body})
-        getLogger().info('Client IP:', request.ip)
-        getLogger().info('Client IPs:', request.ips)
+        getLogger().info({clientIP: request.ip})
+        getLogger().info({clientIPs: request.ips})
         if (!request.isAuthenticated()) {
             return next(new UnauthorizedException('create todo'))
         }
