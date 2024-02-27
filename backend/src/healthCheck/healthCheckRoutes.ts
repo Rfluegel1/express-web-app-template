c5301452334695341da183c8306b4ce7cb7e5ad8
reidfluegel <reid.fluegel@gmail.com>
2024-02-27 12:03:59 -0600

    add open api docs for passport and healthcheck routes

--- backend/src/healthCheck/healthCheckRoutes.ts
+++ backend/src/healthCheck/healthCheckRoutes.ts
@@ -5,6 +5,41 @@ import HealthCheckController from './HealthCheckController'
 const router: Router = Router()
 
 let healthCheckController: HealthCheckController = new HealthCheckController()
+
+/**
+ * @swagger
+ * tags:
+ *   name: HealthCheck
+ *   description: HealthCheck management
+ *
+ * /api/health-check:
+ *   post:
+ *     tags: [HealthCheck]
+ *     summary: Checks for dependency status.
+ *     responses:
+ *       200:
+ *         description: A map of connection statuses.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 result:
+ *                   type: string
+ *                 integrations:
+ *                   type: object
+ *                   properties:
+ *                     database:
+ *                       type: object
+ *                       properties:
+ *                         result:
+ *                           type: string
+ *                           description: The result of the database connection.
+ *                           example: "connected"
+ *                         details:
+ *                           type: string
+ *                           description: The details of the database connection.
+ */
 router.get('/health-check', healthCheckController.healthcheck.bind(healthCheckController))
 
 export = router
--- backend/src/passport/passportRoutes.ts
+++ backend/src/passport/passportRoutes.ts
@@ -4,15 +4,64 @@ import { StatusCodes } from 'http-status-codes';
 
 const router = express.Router();
 
+/**
+ * @swagger
+ * tags:
+ *   name: Passport
+ *   description: Passport management
+ *
+ * /api/login:
+ *   post:
+ *     tags: [Passport]
+ *     summary: Logs in a user.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         form-data:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               username:
+ *                 type: string
+ *                 description: The user's email.
+ *               password:
+ *                 type: string
+ *                 description: The user's password.
+ */
 router.post('/login', passport.authenticate('local', {
     successRedirect: '/',
     failureRedirect: '/login'
 }));
 
+/**
+ * @swagger
+ * /api/session-check:
+ *   get:
+ *     summary: Checks if a session is active.
+ *     tags: [Passport]
+ *     responses:
+ *       200:
+ *         description: A boolean value.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 sessionActive:
+ *                   type: boolean
+ *                   description: A boolean value indicating if a session is active.
+ */
 router.get('/session-check', (req, res, next) => {
 	res.status(StatusCodes.OK).send({ sessionActive: req.isAuthenticated() });
 });
 
+/**
+ * @swagger
+ * /api/logout:
+ *   post:
+ *     tags: [Passport]
+ *     summary: Logs out a user.
+ */
 router.post('/logout', (req, res, next) => {
 	req.logout(function(err) {
 		if (err) {
